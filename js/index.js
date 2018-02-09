5d5085f2e4a13f194245647f3a90cbdf48bc907f
Valerie Seer <seervalerie@gmail.com>
2018-02-09 17:51:09 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  //Header fade in
-  $('.container').hide();
-  $('.container').fadeIn(2000);
+
+  if (window.location.href.search('index.html') > 0) {
+    // Add border under Hello link
+    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
+  } else {
+    // Add border under Resume link and other styles 
+    $('.container__nav').css({'background-color': '#1939D6'});
+    $('.container__nav a').css({'color': '#fff'});
+    $('.container__nav .resume-link').css({'border-bottom': '2px solid #fff'});
+  }
+
 });
