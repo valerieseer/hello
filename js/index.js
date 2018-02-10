70e27d7d7572c3cda4934c21d7dc1fefd39cb362
Valerie Seer <seervalerie@gmail.com>
2018-02-09 18:07:40 -0600

    Fixed logic for border under nav links, more updates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-  if (window.location.href.search('index.html') > 0) {
-    // Add border under Hello link
-    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
-  } else {
+  if (window.location.href.search('resume.html') > 0) {
     // Add border under Resume link and other styles 
     $('.container__nav').css({'background-color': '#1939D6'});
     $('.container__nav a').css({'color': '#fff'});
     $('.container__nav .resume-link').css({'border-bottom': '2px solid #fff'});
+  } else {
+    // Add border under Hello link
+    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
   }
 
 });
