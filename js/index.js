858ca881489b914cac3c926272cfcb1f70ddc8c6
Valerie Seer <seervalerie@gmail.com>
2018-02-12 21:58:38 -0600

    Added fade in scroll functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+  var year = new Date().getFullYear();
+  $('.copyright-date').append(year);
+
   if (window.location.href.search('resume.html') > 0) {
     // Add border under Resume link and other styles 
     $('.container__nav').css({'background-color': '#1939D6'});
@@ -11,3 +14,20 @@ $(document).ready(function() {
   }
 
 });
+
+$(window).scroll(function () {
+
+  // Fade in divs on scroll
+  $('.container__resume-main-work .fade-in').each(function() {
+    var bottomOfObject = $(this).position().top + 120;
+    var bottomOfWindow = $(window).scrollTop() + $(window).height();
+    
+    //Fade in div
+    if (bottomOfWindow > bottomOfObject) {
+        $(this).animate({
+          'opacity': '1'
+        }, 1300);      
+    }  
+  }); 
+
+})
