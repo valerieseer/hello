b1e8c508e2e657784ecb7c380e3e6b5465d73113
Valerie Seer <seervalerie@gmail.com>
2018-02-13 21:12:13 -0600

    Added animations for page transitions

--- js/index.js
+++ js/index.js
@@ -3,31 +3,18 @@ $(document).ready(function() {
   var year = new Date().getFullYear();
   $('.copyright-date').append(year);
 
-  if (window.location.href.search('resume.html') > 0) {
-    // Add border under Resume link and other styles 
+  if (window.location.href.search('index.html') > 0) {
+    // Add border under Hello link
+    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
+  } else if (window.location.href.search('resume.html') > 0) {
+    // Add border under Resume link and blue background
     $('.container__nav').css({'background-color': '#1939D6'});
     $('.container__nav a').css({'color': '#fff'});
     $('.container__nav .resume-link').css({'border-bottom': '2px solid #fff'});
   } else {
-    // Add border under Hello link
-    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
+    // Add border under Portfolio link
+    $('.container__nav .portfolio-link').css({'border-bottom': '2px solid #1939D6'});
   }
 
 });
 
-$(window).scroll(function () {
-
-  // Fade in divs on scroll
-  $('.container__resume-main-work .fade-in').each(function() {
-    var bottomOfObject = $(this).position().top + 120;
-    var bottomOfWindow = $(window).scrollTop() + $(window).height();
-    
-    //Fade in div
-    if (bottomOfWindow > bottomOfObject) {
-        $(this).animate({
-          'opacity': '1'
-        }, 1300);      
-    }  
-  }); 
-
-})
