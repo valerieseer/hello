28ab1a51de80222ffacc50b925c8517de3e9e345
Valerie Seer <seervalerie@gmail.com>
2018-02-20 11:41:17 -0600

    Fixed nav link underline

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
   var year = new Date().getFullYear();
   $('.copyright-date').append(year);
 
-  if (window.location.href.search('index.html') > 0) {
-    // Add border under Hello link
-    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
-  } else if (window.location.href.search('resume.html') > 0) {
+  if (window.location.href.search('resume.html') > 0) {
     // Add border under Resume link and blue background
     $('.container__nav').css({'background-color': '#1939D6'});
     $('.container__nav a').css({'color': '#fff'});
     $('.container__nav .resume-link').css({'border-bottom': '2px solid #fff'});
-  } else {
+  } else if (window.location.href.search('portfolio.html') > 0) {
     // Add border under Portfolio link
     $('.container__nav .portfolio-link').css({'border-bottom': '2px solid #1939D6'});
-  }
+  } else {
+    // Add border under Hello link
+    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
+  } 
 
 });
 
