e6db8bdabb1cb34e7f53e4278467b93fe6ae9598
Valerie Seer <vseer@simpartners.com>
2018-05-11 14:04:33 -0500

    Cleaned up class names and markup

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
+  var primaryColor = '#1939D6';
 
   // Append dynamic copyright year  
   var year = new Date().getFullYear();
   $('.copyright-date').append(year);
 
-  // Update nav and footer styles according to page
+  // Resume page
   if (window.location.href.search('resume.html') > 0) {
-    // Add border under Resume link and blue background
-    $('.container__nav').css({'background-color': '#1939D6'});
-    $('.container__nav a').css({'color': '#fff'});
-    $('.container__nav .resume-link').css({'border-bottom': '2px solid #fff'});
-  } else if (window.location.href.search('portfolio.html') > 0) {
-    // Add border under Portfolio link
-    $('.container__nav .portfolio-link').css({'border-bottom': '2px solid #1939D6'});
-  } else {
-    // Add border under Hello link
-    $('.container__nav .home-link').css({'border-bottom': '2px solid #1939D6'});
-    $('footer').css({'background-color': '#fff', 'color': '#1939D6'});
-    
-    if ($(window).width() <= 768) {
-      $('footer').css({'margin-top': '130px'});
-    }
+    $('nav .resume-link').css({'border-bottom': '2px solid #fff'});
+    $('nav').css({'background-color': primaryColor});
+    $('nav a').css({'color': '#fff'});
   } 
-
+  // Portfolio page
+  else if (window.location.href.search('portfolio.html') > 0) {
+    $('nav .portfolio-link').css({'border-bottom': '2px solid ' + primaryColor});
+  } 
+  // Home page
+  else {
+    $('nav .home-link').css({'border-bottom': '2px solid ' + primaryColor});
+    $('footer').css({'background-color': '#fff', 'color': primaryColor});
+    $('footer p').css({'color': primaryColor});
+  } 
 });
 
