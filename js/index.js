f7246dca057b4d046433fb526c08f7dd0fa599b8
Valerie Seer <vseer@simpartners.com>
2018-05-21 16:45:26 -0500

    Replace jQuery syntax with vanilla JS

--- js/index.js
+++ js/index.js
@@ -1,25 +1,21 @@
-$(document).ready(function() {
-  var primaryColor = '#1939D6';
+const year = new Date().getFullYear();
 
-  // Append dynamic copyright year  
-  var year = new Date().getFullYear();
-  $('.copyright-date').append(year);
+// Append dynamic copyright year  
+document.getElementsByClassName('copyright-date')[0].textContent = year;
 
-  // Resume page
-  if (window.location.href.search('resume.html') > 0) {
-    $('nav .resume-link').css({'border-bottom': '2px solid #fff'});
-    $('nav').css({'background-color': primaryColor});
-    $('nav a').css({'color': '#fff'});
-  } 
-  // Portfolio page
-  else if (window.location.href.search('portfolio.html') > 0) {
-    $('nav .portfolio-link').css({'border-bottom': '2px solid ' + primaryColor});
-  } 
-  // Home page
-  else {
-    $('nav .home-link').css({'border-bottom': '2px solid ' + primaryColor});
-    $('footer').css({'background-color': '#fff', 'color': primaryColor});
-    $('footer p').css({'color': primaryColor});
-  } 
-});
+// Resume page
+if (window.location.href.search('resume.html') > 0) {
+  document.getElementsByClassName('resume-link')[0].classList.add('underlined--white');
+  document.getElementsByTagName('nav')[0].classList.add('bg--blue');
+  document.getElementsByTagName('footer')[0].classList.add('bg--blue');
+} 
+// Portfolio page
+else if (window.location.href.search('portfolio.html') > 0) {
+  document.getElementsByClassName('portfolio-link')[0].classList.add('underlined--blue');
+  document.getElementsByTagName('footer')[0].classList.add('bg--blue');
+} 
+// Home page
+else {
+  document.getElementsByClassName('home-link')[0].classList.add('underlined--blue');
+} 
 
