27ff5061ae93d1cbf076eb89374c99254ef96daf
Valerie Seer <seervalerie@gmail.com>
2019-02-14 13:52:10 -0600

    Rebrand, clean up code

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 const year = new Date().getFullYear();
 
 // Append dynamic copyright year  
-document.getElementsByClassName('copyright-date')[0].textContent = year;
+document.getElementsByClassName("year")[0].textContent = year;
 
 // Resume page
-if (window.location.href.search('resume.html') > 0) {
-  document.getElementsByClassName('resume-link')[0].classList.add('underlined--white');
-  document.getElementsByTagName('nav')[0].classList.add('bg--blue');
-  document.getElementsByTagName('footer')[0].classList.add('bg--blue');
+if (window.location.href.search("resume.html") > 0) {
+  document.getElementsByClassName("resume-link")[0].classList.add("underlined");
+  document.getElementsByTagName("nav")[0].classList.add("bg");
+  document.getElementsByTagName("footer")[0].classList.add("bg");
 } 
 // Portfolio page
-else if (window.location.href.search('portfolio.html') > 0) {
-  document.getElementsByClassName('portfolio-link')[0].classList.add('underlined--blue');
-  document.getElementsByTagName('footer')[0].classList.add('bg--blue');
+else if (window.location.href.search("portfolio.html") > 0) {
+  document.getElementsByClassName("portfolio-link")[0].classList.add("underlined");
+  document.getElementsByTagName("footer")[0].classList.add("bg");
 } 
 // Home page
 else {
-  document.getElementsByClassName('home-link')[0].classList.add('underlined--blue');
+  document.getElementsByClassName("home-link")[0].classList.add("underlined");
 } 
 
