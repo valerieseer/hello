6eb26018502cb57da925911da306cf83b21e0662
Valerie Seer <seervalerie@gmail.com>
2019-02-19 20:16:07 -0600

    Redo resume/about page

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
 const year = new Date().getFullYear();
 
-// Append dynamic copyright year  
+// append dynamic copyright year  
 document.getElementsByClassName("year")[0].textContent = year;
 
-// Resume page
-if (window.location.href.search("resume.html") > 0) {
-  document.getElementsByClassName("resume-link")[0].classList.add("underlined");
-  document.getElementsByTagName("nav")[0].classList.add("bg");
+// about page
+if (window.location.href.search("about.html") > 0) {
+  document.getElementsByClassName("about-link")[0].classList.add("underlined");
   document.getElementsByTagName("footer")[0].classList.add("bg");
 } 
-// Portfolio page
+// portfolio page
 else if (window.location.href.search("portfolio.html") > 0) {
   document.getElementsByClassName("portfolio-link")[0].classList.add("underlined");
   document.getElementsByTagName("footer")[0].classList.add("bg");
 } 
-// Home page
+// home page
 else {
   document.getElementsByClassName("home-link")[0].classList.add("underlined");
 } 
