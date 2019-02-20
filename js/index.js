30e6ec873308edc5e60863d0e89b5a7ce042b67b
Valerie Seer <seervalerie@gmail.com>
2019-02-20 17:37:34 -0600

    Add project link in about page, redo about page using css grid

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-const year = new Date().getFullYear();
+// Append dynamic copyright year  
+(function() {
+  document.getElementsByClassName("year")[0].textContent = new Date().getFullYear();
+})();
 
-// append dynamic copyright year  
-document.getElementsByClassName("year")[0].textContent = year;
-
-// about page
-if (window.location.href.search("about.html") > 0) {
-  document.getElementsByClassName("about-link")[0].classList.add("underlined");
-  document.getElementsByTagName("footer")[0].classList.add("bg");
-} 
-// portfolio page
-else if (window.location.href.search("portfolio.html") > 0) {
-  document.getElementsByClassName("portfolio-link")[0].classList.add("underlined");
-  document.getElementsByTagName("footer")[0].classList.add("bg");
-} 
-// home page
-else {
-  document.getElementsByClassName("home-link")[0].classList.add("underlined");
-} 
+// Underline link for current page
+(function() {
+  // about page
+  if (window.location.href.search("about.html") > 0) {
+    document.getElementsByClassName("about-link")[0].classList.add("underlined");
+    document.getElementsByTagName("footer")[0].classList.add("bg");
+  } 
+  // portfolio page
+  else if (window.location.href.search("portfolio.html") > 0) {
+    document.getElementsByClassName("portfolio-link")[0].classList.add("underlined");
+    document.getElementsByTagName("footer")[0].classList.add("bg");
+  } 
+  // home page
+  else {
+    document.getElementsByClassName("home-link")[0].classList.add("underlined");
+  } 
+})();
 
