f0fcf3efa42751137f824d3804453a805e685b56
Valerie Seer <seervalerie@gmail.com>
2020-01-10 14:43:07 -0600

    Copy and style updates

--- js/index.js
+++ js/index.js
@@ -8,12 +8,6 @@
   // about page
   if (window.location.href.search("about.html") > 0) {
     document.getElementsByClassName("about-link")[0].classList.add("underlined");
-    document.getElementsByTagName("footer")[0].classList.add("bg");
-  } 
-  // portfolio page
-  else if (window.location.href.search("portfolio.html") > 0) {
-    document.getElementsByClassName("portfolio-link")[0].classList.add("underlined");
-    document.getElementsByTagName("footer")[0].classList.add("bg");
   } 
   // home page
   else {
